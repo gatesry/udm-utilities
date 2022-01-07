91691a50360d448836093c6a3e53d49c45e284e8
Ryan Gates <github@rcmhq.com>
2022-01-07 15:35:48 -0600

    final test

--- podman-update/01-podman-update.sh
+++ podman-update/01-podman-update.sh
@@ -11,7 +11,7 @@ CONMON_DL=/mnt/data/.cache/conmon-$CONMON_VERSION
 SECCOMP=/usr/share/containers/seccomp.json
 
 while [ ! -f $CONMON_DL ]; do
-  curl -fsSLo $CONMON_DL https://github.com/gatesry/udm-utilities/blob/master/podman-update/build/conmon-v2.0.30?raw=true
+  curl -fsSLo $CONMON_DL https://github.com/gatesry/udm-utilities/blob/master/podman-update/bin/usr/libexec/podman/conmon?raw=true
   sleep 1
 done
 chmod +x $CONMON_DL
@@ -21,7 +21,7 @@ fi
 ln -s $CONMON_DL /usr/libexec/podman/conmon
 
 if [ ! -f $PODMAN_DL ]; then
-  curl -fsSLo $PODMAN_DL https://github.com/gatesry/udm-utilities/blob/master/podman-update/build/podman-v3.4.2?raw=true
+  curl -fsSLo $PODMAN_DL https://github.com/gatesry/udm-utilities/blob/master/podman-update/bin/usr/bin/podman?raw=true
 fi
 chmod +x $PODMAN_DL
 if [ ! -f /usr/bin/podman.old ]; then
@@ -30,7 +30,7 @@ fi
 ln -s $PODMAN_DL /usr/bin/podman
 
 if [ ! -f $RUNC_DL ]; then
-  curl -fsSLo $RUNC_DL https://github.com/gatesry/udm-utilities/blob/master/podman-update/build/runc-v1.0.2?raw=true
+  curl -fsSLo $RUNC_DL https://github.com/gatesry/udm-utilities/blob/master/podman-update/bin/usr/bin/runc?raw=true
 fi
 chmod +x $RUNC_DL
 if [ ! -f /usr/bin/runc.old ]; then
@@ -40,7 +40,7 @@ ln -s $RUNC_DL /usr/bin/runc
 
 if [ ! -f $SECCOMP ]; then
   mkdir -p /usr/share/containers/
-  curl -fsSLo $SECCOMP https://github.com/gatesry/udm-utilities/blob/master/podman-update/build/seccomp.json?raw=true
+  curl -fsSLo $SECCOMP https://raw.githubusercontent.com/gatesry/udm-utilities/master/podman-update/bin/usr/share/containers/seccomp.json
 fi
 sed -i 's/driver = ""/driver = "overlay"/' /etc/containers/storage.conf
 sed -i 's/ostree_repo = ""/#ostree_repo = ""/' /etc/containers/storage.conf
